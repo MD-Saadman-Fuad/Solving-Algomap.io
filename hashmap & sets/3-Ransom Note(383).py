ccf1ed64dd0eddd3cadcb58e4db4774341bb0aed
MD-Saadman-Fuad <md.saadman.fuad@gmail.com>
2024-12-07 23:45:34 +0600

    with optimal solution

--- hashmap & sets/3-Ransom Note(383).py
+++ hashmap & sets/3-Ransom Note(383).py
@@ -1,5 +1,6 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+# Brute Force
         # ransomeNote = sorted(ransomNote)
         # magazine = sorted(magazine)
         # count = 0
@@ -11,21 +12,30 @@ class Solution:
 
         # return False
 
+# O(R+M)
+        # from collections import Counter
+
+        # counter = Counter(magazine)
+
+        # for i in range(len(ransomNote)):
+        #     if ransomNote[i] in counter:
+        #         if counter[ransomNote[i]] > 0:
+        #             counter[ransomNote[i]] -= 1
+        #         else:
+        #             return False
+        #     else:
+        #         return False
+
+        # return True
+
+#Top Code from leetcode
 
         from collections import Counter
-
-        counter = Counter(magazine)
-
-        for i in range(len(ransomNote)):
-            if ransomNote[i] in counter:
-                if counter[ransomNote[i]] > 0:
-                    counter[ransomNote[i]] -= 1
-                else:
-                    return False
-            else:
-                return False
-
-        return True
+        st1, st2 = Counter(ransomNote), Counter(magazine)
+        if st1 & st2 == st1:
+            # print(st1 & st2)
+            return True
+        return False
 
 s = Solution()
 
