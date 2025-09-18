2b11bbeec7e20516c0421f47bbc80b449e1c6204
MD. Saadman Fuad <md.saadman.fuad@gmail.com>
2025-09-19 01:20:13 +0600

    Extra Problems for Fun

--- 75. Sort Colors.py
+++ 75. Sort Colors.py
@@ -3,6 +3,29 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+
+        numset = {}
+
+        for num in nums:
+            if num in numset:
+                numset[num] +=1
+            else:
+                numset[num] =1
+        
+        for i in range(len(nums)):
+
+            if 0 in numset and numset[0]>0:
+                nums[i] = 0
+                numset[0] = numset[0]-1
+            elif 1 in numset and numset[1]>0:
+                nums[i] = 1
+                numset[1] = numset[1]-1
+            elif 2 in numset and numset[2]>0:
+                nums[i] = 2
+                numset[2] = numset[2] -1
+        return nums
+
+        
         
 
 
--- x^n.py
+++ x^n.py
@@ -1,6 +1,6 @@
 def pow(x, n):
     import math
     return math.pow(x, n)
-
+# to be continued continued
 result = pow(2.000, .001)
 print(result)
